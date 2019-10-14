bfb14b790df7e942d4bc3f66c7027f99ba687e91
Sk Samim Rana <sksamimrana94@gmail.com>
2019-10-14 13:04:46 +0530

    Added config file.

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
+# from flask import Flask
+# from flask import jsonify
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate, MigrateCommand
+# from flask_script import Manager
+
+# from config import settings
+
+# app = Flask(__name__)
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = settings.connection_string
+# db = SQLAlchemy(app)
+
+# migrate = Migrate(app, db)
+# manager = Manager(app)
+# manager.add_command('db', MigrateCommand)
+
+# if __name__ == '__main___':
+#     app.run()
+
+
+import os
+
+from dotenv import load_dotenv
 from flask import Flask
 
 
+BASE_DIR = os.path.join(os.path.abspath('.'), '.env')
+load_dotenv(BASE_DIR)
+
 app = Flask(__name__)
+app.config.from_object(os.environ.get('APP_SETTINGS'))
 
 
 @app.route('/')
 def hello():
-    return "Hello World"
+    return 'Hello World'
 
 
 if __name__ == '__main__':
